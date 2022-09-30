149abd1f2b74e8d40fbaab89bd29097f67007331
Milton <miltonsoftwareengineer@gmail.com>
2022-09-30 05:33:53 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+      <nav>
+        {links.map((link) => {
+          let pageLinks = `#${link}`
+          return <a href={pageLinks} key={Math.random() *10}>
+            {link}
+          </a>
+        })}
+      </nav>
+  );
 }
+
 
 export default NavBar;
